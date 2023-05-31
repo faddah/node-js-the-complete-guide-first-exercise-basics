b98025a9e718703c8f8c1ca3f6dbfe77f72c4de0
Faddah Wolf <my_biz@me.com>
2023-05-31 16:59:39 -0700

    Refactored readFileToArray() to use readFileSync()

--- usersList.js
+++ usersList.js
@@ -1,29 +1,33 @@
 const fs = require('fs');
 
+const pathToUserListFile = './userList.txt';
+
 const readFileToArray = (filePath) => {
-	return new Promise((resolve, reject) => {
-		fs.readFile(filePath, 'utf8', (error, data) => {
-			if (error) {
-				reject(`The Promise to read in the text file was rejected: ${error}.`);
-			} else {
-				const dataArray = data.trim().split('\n');
-				resolve(dataArray);
-			}
-		});
-	});
+	try {
+		const data = fs.readFileSync(filePath, 'utf8');
+		const dataArray = data.trim().split('\n');
+		return dataArray;
+	} catch (error) {
+		throw new Error(`The Promise to read in the text file was rejected: ${error}.`);
+	}
 }
 
-const usersList = readFileToArray('./userList.txt')
-	.then(arrayData => {
-		console.log(arrayData);
-		const lineItems = arrayData.map(user => `<li>${user}</li>\n`).join().toString();
-		console.log(`Here are the returned lineItems: \n${lineItems}.`);
-		return lineItems;
-	})
-	.catch(error => {
-		console.error(`There was an error reading in the User List Text file: ${error}.`);
-	});
+const dataList = readFileToArray(pathToUserListFile);
 
-console.log(`Our Users List: ${usersList}`);
+const usersList = dataList.map(user => `<li>${user}</li>\n`).join('').toString();
 
 module.exports.usersList = usersList;
+
+
+// const readFileToArray = (filePath) => {
+// 	return new Promise((resolve, reject) => {
+// 		fs.readFile(filePath, 'utf8', (error, data) => {
+// 			if (error) {
+// 				reject(`The Promise to read in the text file was rejected: ${error}.`);
+// 			} else {
+// 				const dataArray = data.trim().split('\n');
+// 				resolve(dataArray);
+// 			}
+// 		});
+// 	});
+// }
