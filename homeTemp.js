b5f78a4f78f4e84d6c51ce501b86d9e63a00c667
Faddah Wolf <my_biz@me.com>
2023-05-31 14:03:17 -0700

    Home page template now its own file.

---
+++ homeTemp.js
@@ -0,0 +1,16 @@
+
+const homeTemplate = `
+<html>
+	<head>
+		<title>
+			My First Server Page
+		</title>
+	</head>
+	<body>
+		<h1 style='color: dodgerblue; font-size: 36px; text-align: center;'>
+			Hello from Faddah's Node.JS User List Server!
+		</h1>
+	</body>
+</html>`;
+
+module.exports.homeTemplate = homeTemplate;
