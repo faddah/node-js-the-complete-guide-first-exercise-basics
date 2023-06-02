8356d04ae5df4a0f763c9a8cfa8e59aa72f7a255
Faddah Wolf <my_biz@me.com>
2023-06-01 18:04:42 -0700

    Cleaned up Styles.

--- homeWithInputTemp.js
+++ homeWithInputTemp.js
@@ -4,13 +4,16 @@ const homeWithInputTemplate = `
 		<title>My Enter Message Form Page</title>
 	</head>
 	<body style="margin: 3em">
-		<h1 style = "color: dodgerblue; font-size: 36px; margin-bottom: 7.5%; text-align: center;">
+		<h1 style="color: dodgerblue; font-size: 36px; margin-bottom: 7.5%; text-align: center;">
 			Hello from Faddah's Node.JS User List Server!
 		</h1>
 		<form action="/create-user" method="POST">
 			<input type="text" name="create-user" style="font-size: 24px; width: 35%;">
 			<button type="submit" style="background-color: dodgerblue; border-radius: 15px; color: white; font-size: 24px; font-weight: 700; padding: 0.5em;">Add User Name</button>
 		</form>
+		<div style="color: dodgerblue; font-size: 36px; margin-top: 7.5%; text-align: center;">
+			<a href="/users">Go To Users List</a>
+		</div>
 	</body>
 </html>
 `;
