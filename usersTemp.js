85112b1c5f0dcead6ec2180b9cdc480eacc2e764
Faddah Wolf <my_biz@me.com>
2023-06-04 07:04:49 -0700

    Made usersTemp.js an async/await function

--- usersTemp.js
+++ usersTemp.js
@@ -1,6 +1,7 @@
 const { getUsersList } = require('./usersList')
 
-const usersTemplate = `
+// make getUsersList both a function & async / await so it waits for data list to populate
+const usersTemplate = async () => `
 <html>
 	<head>
 		<title>User List Page</title>
@@ -9,7 +10,7 @@ const usersTemplate = `
 		<div style="color: darkblue; font-family: 'Apple Gothic', Helvetica, Ariel, sans-serif; font-size: 24px; margin: 2.5%;">
 			<h1 style="font-size: 36px; text-align: center";">User List Page</h1>
 			<ul style="font-size: 24px; font-weight: 900;">
-				${getUsersList()}
+				${await getUsersList()}
 			</ul>
 			<a href="/" style="font-weight: 900;"><<= Go back</a>
 		</div>
