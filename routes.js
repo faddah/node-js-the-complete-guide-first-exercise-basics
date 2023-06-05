862cbf2a6a15a170015f6395cc6ac6be371e0b67
Faddah Wolf <my_biz@me.com>
2023-06-05 03:09:35 -0700

    Tav formatting.

--- routes.js
+++ routes.js
@@ -6,49 +6,47 @@ const { homeTemplate } = require('./homeTemp')
 const { usersTemplate } = require('./usersTemp')
 
 const requestHandler = async (req, res) => {
-const url = req.url;
-const method = req.method;
-if (url === '/favicon.ico') {
-	const faviconPath = path.join(__dirname, './', 'favicon.ico');
-	const faviconStream = fs.createReadStream(faviconPath);
-	res.writeHead(200, { 'Content-Type': 'image/x-icon' });
-	faviconStream.pipe(res);
-	// return res.end();
-	return;
-}
-if (url === '/') {
-	res.write(homeWithInputTemplate);
-	return res.end();
-}
-	if (url === '/users') {
-		res.write(await usersTemplate());
+	const url = req.url;
+	const method = req.method;
+	if (url === '/favicon.ico') {
+		const faviconPath = path.join(__dirname, './', 'favicon.ico');
+		const faviconStream = fs.createReadStream(faviconPath);
+		res.writeHead(200, { 'Content-Type': 'image/x-icon' });
+		faviconStream.pipe(res);
+		// return res.end();
+		return;
+	}
+	if (url === '/') {
+		res.write(homeWithInputTemplate);
 		return res.end();
 	}
-	if (url === '/create-user' && method === 'POST') {
-		const body = [];
-		req.on('data', chunk => {
-			console.log(`data chunk in routes.js: ${chunk}`);
-			body.push(chunk);
-		});
-		return req.on('end', async () => {
-			const parsedBody = Buffer.concat(body).toString();
-			console.log(`parsedBody var in routes.js: ${parsedBody}`);
-			const newUser = `\n${parsedBody.split('=')[1].replace(/\+/g, " ")}`;
-			console.log(`newUser var in routes.js: ${newUser}`);
-			fs.appendFileSync('userList.txt', newUser, err => {
-				res.statusCode = 302;
-				res.setHeader('Location', '/users');
-				return res.end();
-			});
-		if (url === '/create-user') {
+		if (url === '/users') {
 			res.write(await usersTemplate());
 			return res.end();
 		}
-		});
-	}
-	res.setHeader('Content-Type', 'text/html');
-	res.write(homeTemplate);
-	res.end();
+		if (url === '/create-user' && method === 'POST') {
+			const body = [];
+			req.on('data', chunk => {
+				body.push(chunk);
+			});
+			return req.on('end', async () => {
+				const parsedBody = Buffer.concat(body).toString();
+				const newUser = `\n${parsedBody.split('=')[1].replace(/\+/g, " ")}`;
+				console.log(`newUser var in routes.js: ${newUser}`);
+				fs.appendFileSync('userList.txt', newUser, err => {
+					res.statusCode = 302;
+					res.setHeader('Location', '/users');
+					return res.end();
+				});
+			if (url === '/create-user') {
+				res.write(await usersTemplate());
+				return res.end();
+			}
+			});
+		}
+		res.setHeader('Content-Type', 'text/html');
+		res.write(homeTemplate);
+		res.end();
 }
 
 module.exports.handler = requestHandler;
